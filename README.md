3e0dedd1dc19597fffd89e00f8459182b942dfee
kaizenacademy <kaizen.techacademy@gmail.com>
2024-07-17 19:22:40 +0000

    new version

--- README.md
+++ README.md
@@ -3,10 +3,14 @@
 ```hcl
 module "vpc" {
     source = "kaizenacademy/vpcbatch4/aws"
+    version = "0.0.2
     region = "us-east-2"
     vpc_cidr = "10.0.0.0/16"
     ip_on_launch = true
-    port = [22, 80, 443]
+    port = [
+        { from_port = 22, to_port = 22},
+        { from_port = 80, to_port = 83}
+    ]
 
     subnet = [
        { cidr = "10.0.1.0/24", subnet_name = "kaizen1" },
--- sg.tf
+++ sg.tf
@@ -3,28 +3,17 @@ resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
 
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = var.port[0]
-    to_port          = var.port[0]
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+    dynamic ingress {
 
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = var.port[1]
-    to_port          = var.port[1]
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+    for_each = var.port
 
-      ingress {
+    content {
     description      = "TLS from VPC"
-    from_port        = var.port[2]
-    to_port          = var.port[2]
+    from_port        = ingress.value.from_port
+    to_port          = ingress.value.to_port
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
+    }
   }
 
   egress {
